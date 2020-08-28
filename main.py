9ee4e0b52e7ed25931154477971bba50ac1c3c14
Julian van Doorn <jvdoorn@antarc.com>
2020-08-28 21:43:08 +0200

    Properly fetches end and begin times.

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ from openpyxl.utils import column_index_from_string
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 SCHEDULE = '/Users/julian/OneDrive - Universiteit Leiden/rooster.xlsx'
 
+BEGIN_TIMES_CAMPUS = ['09:00', '10:00', '11:00', '12:00', '13:30', '14:30', '15:30', '16:30', '17:30']
+END_TIMES_CAMPUS = ['09:45', '10:45', '11:45', '12:45', '14:15', '15:15', '16:15', '17:15', '18:15']
+BEGIN_TIMES_ONLINE = ['09:15', '10:15', '11:15', '12:15', '13:15', '14:15', '15:15', '16:15', '17:15']
+END_TIMES_ONLINE = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00']
+
 LAST_COLUMN = column_index_from_string('AU')
 FIRST_COLUMN = column_index_from_string('C')
 LAST_ROW = 56
 FIRST_ROW = 3
+
+
+class Appointment:
+    def __init__(self, title, type, begin_time, end_time):
+        self.title = title
+        self.type = type
+        self.begin_time = begin_time
+        self.end_time = end_time
 
 
 class AppointmentType(Enum):
@@ -64,6 +77,24 @@ def get_next_cell(cell, ws, check_merged=True):
             return Cell(ws, row=cell.row, column=cell.col_idx + 1)
 
 
+def get_begin_time(cell, appointment_type):
+    if appointment_type is AppointmentType.CAMPUS:
+        return BEGIN_TIMES_CAMPUS[(cell.column - FIRST_COLUMN) % 9]
+    else:
+        return BEGIN_TIMES_ONLINE[(cell.column - FIRST_COLUMN) % 9]
+
+
+def get_end_time(cell, appointment_type, ws):
+    cell_range = get_merged_range(cell, ws)
+    if cell_range:
+        cell = get_last_in_range(cell_range, ws)
+
+    if appointment_type is AppointmentType.CAMPUS:
+        return END_TIMES_CAMPUS[(cell.column - FIRST_COLUMN) % 9]
+    else:
+        return END_TIMES_ONLINE[(cell.column - FIRST_COLUMN) % 9]
+
+
 def get_credentials():
     credentials = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -107,10 +138,34 @@ def main():
     #     print(start, event['summary'])
 
     workbook = load_workbook(SCHEDULE).active
-    next = Cell(workbook, row=FIRST_ROW, column=FIRST_COLUMN)
-    while next:
-        print(f'{next.coordinate}: {workbook[next.coordinate].value} {get_appointment_type(workbook[next.coordinate])}')
-        next = get_next_cell(next, workbook)
+    cell = Cell(workbook, row=FIRST_ROW, column=FIRST_COLUMN)
+    previous_appointments = []
+    while cell:
+        appointment_type = get_appointment_type(workbook[cell.coordinate])
+
+        raw_title = workbook[cell.coordinate].value
+        if raw_title:
+            titles = raw_title.split(' / ')
+
+            appointments = []
+            for title in titles:
+                if title is None:
+                    pass
+
+                for previous_appointment in previous_appointments:
+                    if title == previous_appointment.title:
+                        previous_appointment.end_time = get_end_time(cell, appointment_type, workbook)
+                        appointments.append(previous_appointment)
+                        break
+                appointments.append(Appointment(title, appointment_type, get_begin_time(cell, appointment_type), get_end_time(cell, appointment_type, workbook)))
+
+            previous_appointments = appointments
+            print(f'{appointments[-1].begin_time} - {appointments[-1].end_time} {appointments[-1].title}')
+        else:
+            previous_appointments = []
+
+        # print(f'{cell.coordinate}: {workbook[cell.coordinate].value} {get_appointment_type(workbook[cell.coordinate])}')
+        cell = get_next_cell(cell, workbook)
 
 
 if __name__ == '__main__':
