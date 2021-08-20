fa5d92cf5b8d70576e1204196fa58d8bca3ab8fd
Julian van Doorn <jvdoorn@antarc.com>
2021-08-20 10:53:09 +0200

    Delete appointments in batch.

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Tuple
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build as get_service
+from googleapiclient.http import BatchHttpRequest
 
 from appointment import Appointment
 from config import *
@@ -58,8 +59,16 @@ def save_appointments_to_cache(appointments: List[Appointment]):
 
 
 def delete_remote_appointments(event_ids: list, calendar):
+    batch: BatchHttpRequest = calendar.new_batch_http_request()
+
+    def callback(id, _, exception):
+        if exception is not None:
+            print(f'Failed to delete event: {exception}.')
+
     for event_id in event_ids:
-        delete_appointment(calendar, event_id)
+        batch.add(calendar.events().delete(calendarId=CALENDAR_ID, eventId=event_id), callback)
+
+    batch.execute()
 
 
 def update_end_time(previous_appointment, new_end_time):
@@ -74,11 +83,3 @@ def create_appointment(calendar, appointment):
     except Exception as e:
         print(f'Error creating event {appointment}: {e}.')
         return None
-
-
-def delete_appointment(calendar, event_id):
-    try:
-        calendar.events().delete(calendarId=CALENDAR_ID, eventId=event_id).execute(num_retries=10)
-        print(f'Deleted event with event ID {event_id}.')
-    except Exception as e:
-        print(f'Error deleting event with event ID {event_id}: {e}.')
