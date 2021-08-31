7810ced2381434699b72cdf05025059186ff0bf4
Julian van Doorn <jvdoorn@antarc.com>
2021-08-31 18:10:52 +0200

    Revert "Update google authentication flow."
    This reverts commit d40a214a2e1225f23efbfe4cbe94630039cfab39.

--- utils/google.py
+++ utils/google.py
@@ -1,11 +1,11 @@
 import logging
 import os
+import pickle
 from typing import List
 
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build as get_service
 from googleapiclient.http import BatchHttpRequest
 
 from appointment import Appointment
@@ -15,23 +15,24 @@ from config import CALENDAR_ID, SCOPES
 def get_credentials():
     credentials = None
 
-    if os.path.exists('token.json'):
-        credentials = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             credentials = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(credentials.to_json())
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
 
     return credentials
 
 
 def get_calendar_service():
     credentials = get_credentials()
-    return build('calendar', 'v3', credentials=credentials)
+    return get_service('calendar', 'v3', credentials=credentials)
 
 
 def delete_remote_appointments(event_ids: list, calendar):
